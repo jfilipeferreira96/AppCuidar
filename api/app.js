c0231f11ebce87c67d6428aa8a8d5ae2954bf03d
Filipe Ferreira <filipeferreira96@hotmail.com>
2023-04-22 12:30:19 +0100

    helper: local ipadress for the reactnative

--- api/app.js
+++ api/app.js
@@ -1,17 +1,20 @@
 const port = process.env.PORT || 8080;
-const host = process.env.HOST || '0.0.0.0';
-const express = require('express');
+const host = "127.0.0.1";
+const express = require("express");
 const app = express();
 
-app.use('/assets', express.static('assets'));
-app.use('/views', express.static('views'));
+const os = require("os");
+const networkInterfaces = os.networkInterfaces();
 
+app.use("/assets", express.static("assets"));
+app.use("/views", express.static("views"));
 
-require('./init/db.js')(app, () => {
-  require('./init/middleware')(app);
-  require('./init/router')(app);
+require("./init/db.js")(app, () => {
+  require("./init/middleware")(app);
+  require("./init/router")(app);
   app.listen(port, host, (error) => {
     if (error) throw error;
-    console.log('Your app is listening on ' + port);
+    console.log("Your app is listening on " + port);
+    console.log(`If you are in localhost, use the following ip for the react-native ${networkInterfaces["Ethernet"][3]?.address}:${port}`);
   });
 });
