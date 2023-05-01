fcc175856c5223e13a9044bd23beb24a84dc65e3
Filipe Ferreira <filipeferreira96@hotmail.com>
2023-05-01 16:59:24 +0100

    dotenv package for .env files

--- api/app.js
+++ api/app.js
@@ -9,18 +9,20 @@ const networkInterfaces = os.networkInterfaces();
 app.use("/assets", express.static("assets"));
 app.use("/views", express.static("views"));
 
+require("dotenv").config();
+
 require("./init/db.js")(app, () => {
   require("./init/middleware")(app);
   require("./init/router")(app);
   app.listen(port, host, (error) => {
     if (error) throw error;
     console.log("Your app is listening on " + port);
-    const interfaces = require('os').networkInterfaces();
-    const ethernet = interfaces['Ethernet'];
+    const interfaces = require("os").networkInterfaces();
+    const ethernet = interfaces["Ethernet"];
     if (ethernet && ethernet[3]) {
       console.log(`If you are in localhost, use the following ip for the react-native ${ethernet[3].address}:${port}`);
     } else {
-      console.log('Cannot find Ethernet interface or index 3 is undefined');
+      console.log("Cannot find Ethernet interface or index 3 is undefined");
     }
   });
 });
