c22d41ca06b2f5318a301341782ee6ce6bd57ce3
Filipe Ferreira <filipeferreira96@hotmail.com>
2023-05-02 22:39:02 +0100

    added menu

--- mobile/src/routes/app.routes.js
+++ mobile/src/routes/app.routes.js
@@ -4,31 +4,64 @@ import CareTakerHomeScreen from '../pages/CareTakerHomeScreen';
 import AdminHomeScreen from '../pages/AdminHomeScreen';
 import ListUsers from '../pages/ListUsers';
 
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import AuthContext from '../contexts/auth';
 
+import AuthContext from '../contexts/auth';
+import {useNavigation} from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
 const AppStack = createNativeStackNavigator();
+
+const CustomDrawerContent = props => {
+  const {user, signOut} = useContext(AuthContext);
+  const userType = user?.type;
+  const navigation = useNavigation();
+
+  const HomeString = () => {
+    if (userType === 'user') return 'UserHomeScreen';
+    if (userType === 'caretaker') return 'CareTakerHomeScreen';
+    if (userType === 'admin') return 'AdminHomeScreen';
+  };
+
+  return (
+    <DrawerContentScrollView {...props}>
+      {/* <DrawerItemList {...props} /> */}
+      <DrawerItem
+        label="Home"
+        onPress={() => navigation.navigate(HomeString())}
+      />
+      <DrawerItem label="Logout" onPress={signOut} />
+    </DrawerContentScrollView>
+  );
+};
 
 const AppRoutes = () => {
   const {user} = useContext(AuthContext);
   const userType = user?.type;
 
   return (
-    <AppStack.Navigator>
+    <Drawer.Navigator
+      drawerContent={props => <CustomDrawerContent {...props} />}>
       {userType === 'user' ? (
-        <AppStack.Screen
+        <Drawer.Screen
           name="UserHomeScreen"
           component={UserHomeScreen}
           options={{headerShown: false}}
         />
       ) : userType === 'caretaker' ? (
-        <AppStack.Screen
+        <Drawer.Screen
           name="CareTakerHomeScreen"
           component={CareTakerHomeScreen}
           options={{headerShown: false}}
         />
       ) : userType === 'admin' ? (
-        <AppStack.Screen
+        <Drawer.Screen
           name="AdminHomeScreen"
           component={AdminHomeScreen}
           options={{headerShown: false}}
@@ -41,7 +74,7 @@ const AppRoutes = () => {
         component={ListUsers}
         options={{headerShown: false}}
       />
-    </AppStack.Navigator>
+    </Drawer.Navigator>
   );
 };
 
